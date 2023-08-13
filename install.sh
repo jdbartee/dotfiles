d1198e57815472536951a72af43e9ac8748f5ac1
JD Bartee <jd@bartee.net>
2023-08-13 12:49:26 -0700

    use apt over apt-get

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-if command -v apt-get &> /dev/null; then
-  sudo apt-get update
-  sudo apt-get install -y zsh
-  sudo apt-get install -y ripgrep
+if command -v apt &> /dev/null; then
+  sudo apt update
+  sudo apt install -y zsh
+  sudo apt install -y ripgrep
 fi
